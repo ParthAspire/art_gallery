e5ea6325ec4990158fe6a9e435e05aa57b159f96
parth <parth.mandavia@aspireedge.com>
2023-07-05 18:03:55 +0530

    - implement bottom nav widget

--- lib/app/common/image_constants.dart
+++ lib/app/common/image_constants.dart
@@ -1,6 +1,13 @@
 const _imgPath = 'lib/assets/images';
 const _iconPath = 'lib/assets/icons';
 
+/// bottom nav icons
+const String kIconHomeFilled = '$_iconPath/home_filled_icon.svg';
+const String kIconHomeUnFilled = '$_iconPath/home_unfilled_icon.svg';
+const String kIconProfileFilled = '$_iconPath/profile_filled_icon.svg';
+const String kIconProfileUnFilled = '$_iconPath/profile_unfilled_icon.svg';
+
+
 const String kIconApp ='$_iconPath/app_icon.png';
 const String kIconNextArrow ='$_iconPath/next_arrow_icon.svg';
 const String kIconGoogle ='$_iconPath/google_icon.svg';
--- lib/app/screens/dashboard/bottom_nav/view/bottom_nav_screen.dart
+++ lib/app/screens/dashboard/bottom_nav/view/bottom_nav_screen.dart
@@ -1,4 +1,8 @@
+import 'package:art_gallery/app/common/color_constants.dart';
+import 'package:art_gallery/app/common/image_constants.dart';
 import 'package:art_gallery/app/screens/dashboard/bottom_nav/controller/bottom_nav_controller.dart';
+import 'package:art_gallery/app/widgets/water_drop_bottom_nav/src/bar_item.dart';
+import 'package:art_gallery/app/widgets/water_drop_bottom_nav/src/build_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,6 +11,41 @@ class BottomNavScreen extends GetView<BottomNavController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Obx(() {
+      return Scaffold(
+        body: IndexedStack(
+          index: controller.selectedIndex.value,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Text("Page 10"),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text("Page 2"),
+            ),
+          ],
+        ),
+        bottomNavigationBar: WaterDropNavBar(
+          backgroundColor: Colors.white,
+          waterDropColor: kColorPrimary,
+          onItemSelected: (index) {
+            controller.selectedIndex.value = index;
+          },
+          selectedIndex: controller.selectedIndex.value,
+          barItems: [
+            BarItem(
+              filledIcon: kIconHomeFilled,
+              outlinedIcon: kIconHomeUnFilled,
+              imgHeight: 38,
+            ),
+            BarItem(
+              filledIcon: kIconProfileFilled,
+              outlinedIcon: kIconProfileUnFilled,
+            )
+          ],
+        ),
+      );
+    });
   }
 }
